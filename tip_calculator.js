b61455e352c1a55ea2469ba9741c021cd5109399
Altemush Bhatti <Altemush.X.Bhatti@kp.org>
2019-11-06 14:14:45 -0800

    Added function to calculate tips and display result in alert

--- tip_calculator.js
+++ tip_calculator.js
@@ -1,6 +1,28 @@
 function tipCalculator(bill) {
+    var percentage;
     if(bill < 50)
     {
-        
+        percentage = .2;
     }
+    else if(bill >= 50 && bill < 200)
+    {
+        percentage = .15;
+    }
+    else {
+        percentage = .1;
+    }
+    return percentage * bill;
 }
+
+var val = document.querySelector("#bill").value;
+
+
+
+
+
+document.querySelector("#calc").addEventListener("click",function() {
+    var val = parseInt(document.querySelector("#bill").value);
+    var tip = tipCalculator(val);
+    alert("Tip value is " + tip);
+});
+
